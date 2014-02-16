79546c7e2cebea20ac50e099090217d758693314
darkspring1 <olt.egor@gmail.com>
2014-02-16 19:01:26 +0400

    fix: получить список детей по родетелю

---
+++ ServerWebApi/api/ChildController.cs
@@ -0,0 +1,20 @@
+﻿using ServerWebApi.Data;
+using ServerWebApi.Data.Entity;
+using System.Web.Http;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ServerWebApi.Controllers
+{
+    public class ChildController : ApiController
+    {
+        Repository<Child> _childRepo = new Repository<Child>();
+
+        [HttpGet]
+        public IEnumerable<Child> Get(string ParentId)
+        {
+            return from child in _childRepo.GetAll()
+                   select child;
+        }
+    }
+}
