710e02631b29e10dfd661d96f49cec7dca73fa91
ds <dsdevelops@gmail.com>
2014-02-16 15:54:11 +0400

    base

---
+++ ChildControl/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ChildControl.Resources;
+
+namespace ChildControl
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
